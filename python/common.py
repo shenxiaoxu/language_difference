5c42c481f4cb816b18908c5a3df961feec158b3b
Xiaoxu Shen <shenxiaoxu22@gmail.com>
2020-12-07 10:19:19 -0800

    Update common.py

--- python/common.py
+++ python/common.py
@@ -1,5 +1,11 @@
+import heapq
 q = []
-heapq.push(q,(1,2))
-heapq.pop(q)
+heapq.heappush(q,(1,2))
+heapq.heappop(q)
 nums =[2,1,3]
 nums.sort()
+s = ["a","b","c"]
+#print(set(zip(nums, s)))
+z = list(zip(nums, s))
+uz, us = zip(*z)
+print(uz) 
